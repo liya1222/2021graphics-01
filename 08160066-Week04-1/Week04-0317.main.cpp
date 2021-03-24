860ce70f1944c29769629809bd402dc415898732
liya1222 <80370207+liya1222@users.noreply.github.com>
2021-03-24 12:08:18 +0800

    Add files via upload

--- 08160066-Week04-1/Week04-0317.main.cpp
+++ 08160066-Week04-1/Week04-0317.main.cpp
@@ -1,16 +1,25 @@
 #include <GL/glut.h>
 int N=0, vx[3000],vy[3000];
+float angle=0;
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glBegin(GL_LINE_LOOP);
-    for(int i=0; i<N; i++)
-    {
-        glVertex2f((vx[i]-150)/150.0, -(vy[i]-150)/150.0);
-    }
-    glEnd();
+    glPushMatrix();///³Æ¥÷¯x°}
+        glRotatef(angle, 0, 0, 1);///±ÛÂà
+        glBegin(GL_LINE_LOOP);
+        for(int i=0; i<N; i++)
+        {
+            glVertex2f((vx[i]-150)/150.0, -(vy[i]-150)/150.0);
+        }
+        glEnd();
+    glPopMatrix();
     glutSwapBuffers();
+}
+void keyboard( unsigned char key, int x, int y)
+{
+    angle+=5;
+    display();
 }
 void motion(int x, int y)
 {
@@ -23,7 +32,9 @@ int main(int argc, char ** argv)
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
     glutCreateWindow("08160066");
+
     glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
     glutMotionFunc(motion);
     glutMainLoop();
 }
